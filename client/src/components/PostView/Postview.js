1ceab4c671e8489c0918df605bef5f406b168df5
soumya13042000 <soumyashreebaral086@gmail.com>
2023-03-04 15:30:21 +0530

    made some changes

--- client/src/components/PostView/Postview.js
+++ client/src/components/PostView/Postview.js
@@ -7,13 +7,13 @@ import axios from "axios";
 
 
 function Postview() {
-    const [APIData, setAPIData] = useState([]);
+    const [data, setData] = useState([]);
     useEffect(() => {
         // axios.get("https://instaclone-10x-back-end.herokuapp.com/").then((res)=>{
         // axios.get("http://localhost:4000/home").then((res)=>{
         axios.get("https://instaclone-backend-4cuf.onrender.com/home").then((res)=>{
             let data = res.data.reverse()
-            setAPIData(data)
+            setData(data)
         }).catch((err)=>{
             console.log(err)
         })
@@ -29,20 +29,20 @@ function Postview() {
              <Link to="/formpage"><span className="material-symbols-outlined">+</span></Link>  
             </header>
             <div className='Cart'> 
-                {APIData.map((data, index) => {
+                {data.map((dt, index) => {
                     return (
                         <ul key={index} className='Cart_Child'>
                             <li className='row-1 '>
                                 <div className='Margin'>
-                                    <h3>{data.Author}</h3>
-                                    <span>{data.Location}</span>
+                                    <h3>{dt.Author}</h3>
+                                    <span>{dt.Location}</span>
                                 </div>
                                 <span className="material-symbols-outlined Margin">
                                     ...
                                 </span>
                             </li>
                             <li className='row-2'>
-                                <img src={data.image} alt='Post' />
+                                <img src={dt.image} alt='Post' />
                             </li>
                             <li className='row-1'>
                                 <div className='row3'>
@@ -52,12 +52,12 @@ function Postview() {
                                     <span className="material-symbols-rounded">
                                         <img src="arro.png" alt="img" id="heart"/>
                                     </span>
-                                    <h6>{data.likes}Likes</h6>
+                                    <h6>{dt.likes}Likes</h6>
                                  </div>
-                                 <span className='Date'>{data.date}</span>
+                                 <span className='Date'>{dt.date}</span>
                              </li>
                              <li>
-                                 <h5 className='des row4'>{data.Description}</h5>
+                                 <h5 className='des row4'>{dt.Description}</h5>
                              </li>
                          </ul>
                     )
